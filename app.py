d2608394551da65311da8c6a10702f3907b69e8b
pandyandy <144467422+pandyandy@users.noreply.github.com>
2024-04-17 18:35:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ if selected_author != 'All':
 
 st.markdown(f"<br>**{title_text}**", unsafe_allow_html=True)
 
-wordcloud = WordCloud(width=1000, height=500, background_color=None, mode="RGBA", colormap=colormap).generate_from_frequencies(word_freq)
+wordcloud = WordCloud(width=800, height=400, background_color=None, mode="RGBA", colormap=colormap).generate_from_frequencies(word_freq)
 wordcloud_array = wordcloud.to_array()
 
 plt.figure(figsize=(10, 5), frameon=False)
